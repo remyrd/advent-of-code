50072baef38583086186920f1b68177f3882833b
Remy Rojas <remy@rojas.cz>
2020-12-25 16:06:46 +0100

    Day 18

---
+++ src/advent_of_code/day18.clj
@@ -0,0 +1,59 @@
+(ns advent-of-code.day18
+  (:require [clojure.string :as string]))
+
+
+(def example "5 + (8 * 3 + 9 + 3 * 4 * 3)")
+
+;; isolate priority operations
+(defn isolate-priority [coll]
+  (let [partitioned (partition-by (partial = '(quote *)) coll)]
+    (if (= coll (first partitioned))
+      coll
+      (reduce (fn [res part]
+                (if (= 1 (count part))
+                  (concat res part)
+                  (concat res (list part))))
+              []
+              partitioned))))
+
+;; only deal with expr without parens
+(defn resolve-internal [coll]
+  (loop [c (isolate-priority coll)] ;;2nd star. use [c coll] instead for 1st star
+    (let [[a x b & r] c
+          resolved-a (if (seq? a)
+                       (resolve-internal a)
+                       a)
+          resolved-b (if (seq? b)
+                       (resolve-internal b)
+                       b)
+          resolved-x (resolve (eval x))
+          res (resolved-x resolved-a resolved-b)]
+      (if (empty? r)
+        res
+        (recur (cons res r))))))
+
+(defn solve-line [line]
+  (->> line
+       (concat "'(")
+       (#(concat % [\)]))
+       (map #(case %
+               (\+ \*) (str "'" %)
+               %))
+       (apply str)
+       read-string
+       ;; line is evaled and parenthesis are semantically accurate
+       ;; embedded parens translate to embedded lists
+       ;; no need to explicitly parse any number B^)
+       eval
+       resolve-internal
+       ))
+
+(comment 
+  (->>
+   "resources/day18"
+   slurp
+   ;; example
+   string/split-lines
+   (map solve-line)
+   (apply +)
+   ))
