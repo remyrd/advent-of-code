8f35f8fad99c18b3a5fa00b4afceb11704fe052c
Remy Rojas <remy@rojas.cz>
2020-12-29 17:47:38 +0100

    Day 19

--- src/advent_of_code/day19.clj
+++ src/advent_of_code/day19.clj
@@ -1,96 +1,46 @@
 (ns advent-of-code.day19
   (:require [clojure.string :as string]))
 
-(def example
-  "42: 9 14 | 10 1
-9: 14 27 | 1 26
-10: 23 14 | 28 1
-1: a
-11: 42 31
-5: 1 14 | 15 1
-19: 14 1 | 14 14
-12: 24 14 | 19 1
-16: 15 1 | 14 14
-31: 14 17 | 1 13
-6: 14 14 | 1 14
-2: 1 24 | 14 4
-0: 8 11
-13: 14 3 | 1 12
-15: 1 | 14
-17: 14 2 | 1 7
-23: 25 1 | 22 14
-28: 16 1
-4: 1 1
-20: 14 14 | 1 15
-3: 5 14 | 16 1
-27: 1 6 | 14 18
-14: b
-21: 14 1 | 1 14
-25: 1 1 | 1 14
-22: 14 14
-8: 42
-26: 14 22 | 1 20
-18: 15 15
-7: 14 5 | 1 21
-24: 14 1
-
-abbbbbabbbaaaababbaabbbbabababbbabbbbbbabaaaa
-bbabbbbaabaabba
-babbbbaabbbbbabbbbbbaabaaabaaa
-aaabbbbbbaaaabaababaabababbabaaabbababababaaa
-bbbbbbbaaaabbbbaaabbabaaa
-bbbababbbbaaaaaaaabbababaaababaabab
-ababaaaaaabaaab
-ababaaaaabbbaba
-baabbaaaabbaaaababbaababb
-abbbbabbbbaaaababbbbbbaaaababb
-aaaaabbaabaaaaababaa
-aaaabbaaaabbaaa
-aaaabbaabbaaaaaaabbbabbbaaabbaabaaa
-babaaabbbaaabaababbaabababaaab
-aabbbbbaabbbaaaaaabbbbbababaaaaabbaaabba")
-
-
-;; when a rule is completely dereffed into lists of literals
-;; concatenate all possible literals so upper level lists can
-;; deref the current one
-(defn all-concats [coll]
-  (reduce (fn [amon aval]
-            (for [a amon
-                  b aval]
-              (str a b))) (first coll) (rest coll)))
-
-(defn generate-matches [line-n rules-map]
-  (let [line (get rules-map line-n "")]
-    (if (every? (partial re-matches #"^[ab]+$") (apply concat line))
-      (apply concat line) ;; all possible combinations of "a" / "b" 
-      (->> line
-           (mapcat (fn [ordered] ;; sets of substrings waiting to be concated
-                     (->> ordered
-                          (map #(if (list? %) ;; deref all numbers
-                                  %
-                                  (generate-matches % rules-map)))
-                          all-concats)))))))
-
 (defn super-partition [v]
   (->> v
        (partition-by (partial = "|"))
        (remove (partial = '("|")))))
 
-(defn solve [rule-lines lines]
+(defn coll-to-int [coll]
+  (map #(case %
+          ("a" "b") (first %)
+          (Integer/parseInt %))
+       coll))
+
+(defn solve-for-line [remainders rule rules-map]
+  (let [current-rule (get rules-map rule)]
+    (if (char? (first (first current-rule)))
+      (for [remainder remainders
+            :let [[c & r] remainder]
+            :when (= c (first (first current-rule)))]
+        r)
+      (for [remainder remainders
+            seq-rule current-rule
+            r (reduce #(solve-for-line % %2 rules-map) (list remainder) seq-rule) ]
+        r))))
+
+(defn solve [rule-lines all-lines]
   (let [rules-map (->> (string/split-lines rule-lines)
                        (map (partial re-seq #"[\d\|ab]+"))
-                       (map (juxt first (comp super-partition rest)))
-                       (into {})
-                       )]
-    (-> (set (generate-matches "0" rules-map))
-        (filter (string/split-lines lines)))))
+                       (map (juxt (comp #(Integer/parseInt %) first)
+                                  (comp (partial map coll-to-int) super-partition rest)))
+                       (into {}))
+        lines (string/split-lines all-lines)]
+    (->> (for [line lines]
+           (solve-for-line (list line) 0 rules-map))
+         (remove empty?)
+         (filter (comp nil? first))
+         count)))
 
 (comment
   (->>
-   ;; "resources/day19"
-   ;; slurp
-   example
+   "resources/day19"
+   slurp
    (#(string/split % #"\n\n"))
    (apply solve)
    ))
