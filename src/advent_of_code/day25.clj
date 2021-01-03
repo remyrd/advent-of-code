42cfaa46068304fd272babf6c5892651e292fd32
Remy Rojas <remy@rojas.cz>
2021-01-03 19:57:35 +0100

    Day 25

---
+++ src/advent_of_code/day25.clj
@@ -0,0 +1,17 @@
+(ns advent-of-code.day25
+  (:require [clojure.string :as string]))
+
+(defn calc [subject]
+  (iterate #(mod (* % subject) 20201227) 1))
+
+(defn get-loops [pk subject]
+  (count (take-while (partial not= pk) (calc subject))))
+
+(defn get-secret [loops subject]
+  (last (take (inc loops) (calc subject))))
+
+(comment
+  (->> (slurp "resources/day25")
+       string/split-lines
+       (map #(Integer/parseInt %))
+       (apply #(get-secret (get-loops % 7) %2))))
