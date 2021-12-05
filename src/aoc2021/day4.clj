f09581d34fb487ba7136e735ce86ad1d1c05fb97
Remy Rojas <remy@rojas.cz>
2021-12-05 11:55:11 +0100

    2021 day5

--- src/aoc2021/day4.clj
+++ src/aoc2021/day4.clj
@@ -1,27 +1,7 @@
 (ns aoc2021.day4
-  (:require [common.util :refer [parse-lines parse-example]]
+  (:require [common.util :refer [parse-lines]]
             [clojure.string :as str]
             [clojure.set :as set]))
-
-(def example (parse-example "7,4,9,5,11,17,23,2,0,14,21,24,10,16,13,6,15,25,12,22,18,20,8,19,3,26,1
-
-22 13 17 11  0
- 8  2 23  4 24
-21  9 14 16  7
- 6 10  3 18  5
- 1 12 20 15 19
-
- 3 15  0  2 22
- 9 18 13 17  5
-19  8  7 25 23
-20 11 10 24  4
-14 21 16 12  6
-
-14 21 17 24  4
-10 16 15  9 19
-18  8 23 26 20
-22 11 13  6  5
- 2  0 12  3  7"))
 
 (def input (parse-lines "resources/2021/day4"))
 
@@ -102,8 +82,4 @@
 (->> boards
      (play-bingo-to-lose numbers)
      ((juxt sum-unmarked-numbers (fn [[_ _ n]] (read-string n))))
-     (apply *)) ;; 1st star
-;; board of NxN
-;; repr as list of numbers 1..N^2
-
-;; (defn win-condition [pos])
+     (apply *)) ;; 2nd star
---
+++ src/aoc2021/day5.clj
@@ -0,0 +1,45 @@
+(ns aoc2021.day5
+  (:require [common.util :refer [parse-lines]]))
+
+(def input (parse-lines "resources/2021/day5"))
+
+(defn parse-line [text-line]
+  (mapv read-string (re-seq #"\d+" text-line)))
+
+(defn vertical-or-horizontal? [[x1 y1 x2 y2]]
+  (or (= x1 x2) (= y1 y2)))
+
+(defn add-points [t points]
+  (if (empty? points)
+      t
+      (let [[p & ps] points]
+        (recur (update-in t p #(if % (inc %) 1))
+               ps))))
+
+(defn make-range [point1 point2]
+  (cond
+    (= point1 point2) (repeat point1)
+    (< point1 point2) (range point1 (inc point2))
+    (> point1 point2) (reverse (range point2 (inc point1)))))
+
+;; matrix represented as a tree of y->x
+(defn build-matrix [lines]
+  (reduce (fn [t [x1 y1 x2 y2]]
+            (add-points t (map vector
+                               (make-range x1 x2)
+                               (make-range y1 y2))))
+          {} lines))
+
+(defn get-result [m]
+  (->> m vals (map vals) flatten (filter #(< 1 %)) count))
+
+(->> input
+     (map parse-line)
+     (filter vertical-or-horizontal?)
+     build-matrix
+     get-result)
+
+(->> input
+     (map parse-line)
+     build-matrix
+     get-result)
